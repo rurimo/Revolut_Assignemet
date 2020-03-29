7e733ecbd737c1eaadab1d01e4bdf4e3378619ad
Mohamed el Amine Benallouch <amine.benallouch@assistme.io>
2020-03-29 18:33:45 +0100

    add ui elements for the main screen

--- app/src/main/java/com/benallouch/revolut/MainActivity.kt
+++ app/src/main/java/com/benallouch/revolut/MainActivity.kt
@@ -1,12 +1,21 @@
 package com.benallouch.revolut
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.benallouch.revolut.view.ui.rates.RatesFragment
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        initializeUI()
+    }
+
+    private fun initializeUI() {
+        supportFragmentManager.beginTransaction().run {
+            replace(R.id.fragmentHolder, RatesFragment(), RatesFragment.FRAGMENT_TAG)
+            commit()
+        }
     }
 }
--- app/src/main/java/com/benallouch/revolut/di/ViewModelModule.kt
+++ app/src/main/java/com/benallouch/revolut/di/ViewModelModule.kt
@@ -1,10 +1,10 @@
 package com.benallouch.revolut.di
 
-import com.benallouch.revolut.view.ui.RatesActivityViewModel
+import com.benallouch.revolut.view.ui.rates.RatesFragmentViewModel
 import org.koin.android.viewmodel.dsl.viewModel
 import org.koin.dsl.module
 
 
 val viewModelModule = module {
-    viewModel { RatesActivityViewModel(get()) }
+    viewModel { RatesFragmentViewModel(get()) }
 }
