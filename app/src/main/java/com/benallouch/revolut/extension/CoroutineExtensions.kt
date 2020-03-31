977bcce3382b97988ae457a3ab398ccfb79a7302
Mohamed el Amine Benallouch <amine.benallouch@assistme.io>
2020-03-31 17:54:18 +0100

    use inline functions

--- app/src/main/java/com/benallouch/revolut/extension/CoroutineExtensions.kt
+++ app/src/main/java/com/benallouch/revolut/extension/CoroutineExtensions.kt
@@ -4,8 +4,9 @@ import kotlinx.coroutines.GlobalScope
 import kotlinx.coroutines.delay
 import kotlinx.coroutines.launch
 
-fun startCoroutineTimer(
-    delayMillis: Long = 1000, repeatMillis: Long = 1000, action: () -> Unit
+//inline function prevents the overhead of creating an object function each time we use it
+inline fun startCoroutineTimer(
+    delayMillis: Long = 1000, repeatMillis: Long = 1000, crossinline action: () -> Unit
 ) = GlobalScope.launch {
     delay(delayMillis)
     if (repeatMillis > 0) {
